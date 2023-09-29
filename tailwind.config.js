0255f4e687992bda3f66223efb46ed57b592d98f
Bryan <bryan.ferrando@epitech.eu>
2023-09-29 08:23:44 +0100

    Added new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,6 +10,9 @@ export default {
         backgroundsec: '#3b424c',
         primary: '#c3f012',
         second: '#4d6325'
+      },
+      fontFamily: {
+        sans: ['Kanit', ...defaultTheme.fontFamily.sans]
       }
     },
   },
